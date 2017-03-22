995e04a6810c1dfc81c8f7082ce2ed0a9ab83979
ccgamble <christine.c.gamble@gmail.com>
2017-03-22 12:14:29 -0600

    Adds coordinates and name for all points

---
+++ scripts.js
@@ -0,0 +1,297 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiZ2FtYmxlY2MiLCJhIjoiY2owbDgwZmxuMDJwNTMzcGtsbXUyOHNkcSJ9.11Kjh-0160rgp8aAAXnyVA';
+var geojson = [
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.8231, 40.2522]
+    },
+    properties: {
+      title: 'Grand Lake',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.4198, 39.8414]
+    },
+    properties: {
+      title: 'Horseshoe Trail, Golden Gate Canyon State Park',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.334167, 39.953889]
+    },
+    properties: {
+      title: 'Walker Ranch',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.2941, 39.9290]
+    },
+    properties: {
+      title: 'Rattlesnake Gulch Trail, Eldorado Canyon State Park',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.3608, 39.5825]
+    },
+    properties: {
+      title: 'Maxwell Falls',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.6416, 40.2894]
+    },
+    properties: {
+      title: 'Mills Lake, RMNP',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.2881, 39.6553]
+    },
+    properties: {
+      title: "O'Fallon Park",
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.6686, 39.5828]
+    },
+    properties: {
+      title: 'Mount Bierstadt',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.2552, 39.7732]
+    },
+    properties: {
+      title: 'Mount Galbraith',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.5981, 39.6604]
+    },
+    properties: {
+      title: 'Echo Lake',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-106.8317, 40.4850]
+    },
+    properties: {
+      title: 'Steamboat Springs',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.6469, 39.8358]
+    },
+    properties: {
+      title: "Saint Mary's Glacier",
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.3075, 40.0017]
+    },
+    properties: {
+      title: 'Lost Gulch Lookout, Flagstaff Mountain',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.3687, 39.6697]
+    },
+    properties: {
+      title: 'Elk Meadow Park',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.6482, 40.3134]
+    },
+    properties: {
+      title: 'Bear Lake, RMNP',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-104.9172, 39.1222]
+    },
+    properties: {
+      title: 'Palmer Lake',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.3537, 39.6250]
+    },
+    properties: {
+      title: 'Alderfer/Three Sisters Park',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.101844, 39.260394]
+    },
+    properties: {
+      title: "Devil's Head Lookout",
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.3057, 39.5203]
+    },
+    properties: {
+      title: 'Meyer Ranch Open Space, Conifer',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-108.6939, 39.0575]
+    },
+    properties: {
+      title: 'Colorado National Monument',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.3267, 39.6340]
+    },
+    properties: {
+      title: 'Evergreen',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.2780, 39.8304]
+    },
+    properties: {
+      title: 'White Ranch Open Space',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-107.1918, 39.6014]
+    },
+    properties: {
+      title: 'Hanging Lake',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.7567, 39.4133]
+    },
+    properties: {
+      title: 'Kenosha Pass',
+      'marker-color': '#3ca0d3',
+      'marker-size': 'small',
+    }
+  },
+];
+
+var mapGeo = L.mapbox.map('map_geo', 'mapbox.light')
+  .setView([39.00 ,-105.5], 7);
+
+var myLayer = L.mapbox.featureLayer().setGeoJSON(geojson).addTo(mapGeo);
+mapGeo.scrollWheelZoom.disable();
